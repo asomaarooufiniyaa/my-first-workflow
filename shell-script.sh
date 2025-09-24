9441d6f87e592d6bccabaa8c9dd06fdf772998ee
Aso Maaroufiniyaa <162102797+asomaarooufiniyaa@users.noreply.github.com>
2025-09-24 12:57:18 +0330

    added-permision

--- shell-script.sh
+++ shell-script.sh
@@ -1,6 +1,6 @@
 sudo apt-get install cowsay -y
-           cowsay -f dragon "my name is aso and im a dragon" >> dragon.txt
-           cat dragon.txt
-           echo "hi this is my first github actions"
-           ls -la
-           cat README.md
+cowsay -f dragon "my name is aso and im a dragon" >> dragon.txt
+cat dragon.txt
+echo "hi this is my first github actions"
+ls -la
+cat README.md
